30c2281f65d960381fe38163e9f22ba1643fbf1f
radriann21 <adrianrangelargu@gmail.com>
2024-11-08 22:46:06 -0400

    finished project

--- src/main.ts
+++ src/main.ts
@@ -5,3 +5,25 @@ const binaryInput:HTMLElement | null = $('#binary')
 const transformButton:HTMLElement | null = $('#transform')
 const resultText:HTMLElement | null = $('#result')
 
+let binaryValue:string 
+
+binaryInput?.addEventListener('change', (evt:Event) => {
+  const target = evt.target as HTMLInputElement
+  binaryValue = target.value
+})
+
+transformButton?.addEventListener('click', () => {
+
+  let result:number = 0
+  let binaryLoop:string = binaryValue.split('').reverse().join('')
+
+  for (let i = 0; i < binaryLoop.length; i++) {
+    let bit = parseInt(binaryLoop[i], 10)
+    let bitValue = bit * Math.pow(2, i)
+    result += bitValue
+  }
+
+  if (resultText !== null) {
+    resultText.innerText = result.toString()
+  }
+})
